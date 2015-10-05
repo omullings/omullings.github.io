ca2a5f1fd3c6534d0bc3848dfb27d3dc42d67ff5
omullings <owen.c.mullings.jr@gmail.com>
2015-10-05 19:03:16 -0400

    added icons by status

--- js/maps/leaf-demo.js
+++ js/maps/leaf-demo.js
@@ -12,16 +12,44 @@ L.tileLayer( 'http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
 
 var myURL = jQuery( 'script[src$="leaf-demo.js"]' ).attr( 'src' ).replace( 'leaf-demo.js', '' );
 
-var myIcon = L.icon({
-    iconUrl: myURL + 'images/pin24.png',
-    iconRetinaUrl: myURL + 'images/pin48.png',
-    iconSize: [24, 24],
-    iconAnchor: [9, 21],
-    popupAnchor: [0, -14]
-});
 
 for ( var i=0; i < markers.length; ++i )
 {
+  var application_status = markers[i]['application_status'];
+
+  var online_status = 1 ;
+  var down_status = 2 ;
+  var offline_status = 3 ;
+
+
+    if( application_status === online_status ){
+    var myIcon = L.icon({
+        iconUrl: myURL + 'images/up.svg',
+        iconRetinaUrl: myURL + 'images/up.svg',
+        iconSize: [24, 24],
+        iconAnchor: [9, 21],
+        popupAnchor: [0, -14]
+    });
+  } else if (application_status ===  down_status ) {
+    var myIcon = L.icon({
+        iconUrl: myURL + 'images/down.svg',
+        iconRetinaUrl: myURL + 'images/down.svg',
+        iconSize: [24, 24],
+        iconAnchor: [9, 21],
+        popupAnchor: [0, -14]
+    });
+  } else if (application_status ===  offline_status ) {
+    var myIcon = L.icon({
+        iconUrl: myURL + 'images/offline.svg',
+        iconRetinaUrl: myURL + 'images/offline.svg',
+        iconSize: [24, 24],
+        iconAnchor: [9, 21],
+        popupAnchor: [0, -14]
+    });
+  } else {
+
+  }
+
    L.marker( [markers[i].lat, markers[i].lng], {icon: myIcon} )
       .bindPopup( '<a href="' + markers[i].url + '" target="_blank">' + markers[i].name + '</a>' )
       .addTo( map );
